5edd634a8f63751052d3fd868790bdcfbabcbc34
Jônatas Mendes da Cruz <62781857+Kyoto-01@users.noreply.github.com>
2022-05-16 11:00:55 -0300

    Update lib.js

--- src/lib.js
+++ src/lib.js
@@ -1,19 +1,28 @@
-import { promisify } from 'util';
-import { exec } from 'child_process';
-
-
-const execAsync = promisify(exec);
+import si from 'systeminformation';
 
 async function get_cpu_infos(){
-    const { stdout } = await execAsync('lscpu');
-
-    return stdout;
+    const data = await si.cpu();
+    return format_infos(data, 'CPU');
 }
 
 async function get_memory_infos(){
-    const { stdout } = await execAsync('lsmem');
+}
 
-    return stdout;
+function format_infos(infosObj, infoName){
+    let infoStr = infoName ? `<h1>${infoName}</h1><ul>` : '<ul>';
+
+    for (let key in infosObj){
+        if (typeof(infosObj[key]) == 'object'){
+            infoStr += `<li>${key} --> ${format_infos(infosObj[key])}</li>`;
+        }
+        else{
+            infoStr += `<li>${key} --> ${infosObj[key]}</li>`;
+        }
+    }
+
+    infoStr += '</ul>';
+
+    return infoStr;
 }
 
 export default { get_cpu_infos, get_memory_infos };
